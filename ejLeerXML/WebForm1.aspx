64afa4c21e7e5de9e8de5f9baf28403d277dcabf
Marco Chávez <totto_cha@hotmail.com>
2015-10-04 01:52:06 -0600

    Ejemplo
    Leer archivo xml c# asp.net

---
+++ ejLeerXML/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ejLeerXML.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Leer archivo XML</h1>
+            <asp:Button Class="Boton" ID="Button1" runat="server" Text="Leer" OnClick="Button1_Click" />
+            <asp:TextBox ID="txtlectura" runat="server" TextMode="MultiLine"></asp:TextBox>
+        </div>
+    </form>
+    
+</body>
+</html>
---
+++ ejLeerXML/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace ejLeerXML
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlTextReader TR = new XmlTextReader("C:\\Users\\u\\Visual Studio 2013\\ejLeerXML\\ejLeerXML\\definicion.xml"); //Crea un XmlTextReader abriendo el archivo
+            string ultimaEtiqueta = ""; //cadena que almacena la ultima etiqueta
+            while (TR.Read()) //ciclo mientras XmlTextReader está leendo
+            {
+                if (TR.NodeType == XmlNodeType.Element) //condicional cuando lee un tipo elemento
+                {
+                    txtlectura.Text += (new String(' ', TR.Depth * 3) + "<" + TR.Name + ">");
+                    ultimaEtiqueta = TR.Name;
+                    continue;
+                }
+                if (TR.NodeType == XmlNodeType.Text) //condicional cuando lee un tipo texto
+                {
+                    txtlectura.Text += TR.ReadContentAsString() + "</" + ultimaEtiqueta + ">";
+                }
+                else //de lo contrario, será un salto de línea
+                    txtlectura.Text += "\r";
+            }
+        }
+
+    }
+}
